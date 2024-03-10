7d88aca6cb01f05558106d84035a42ad15ca83fb
Roman Freiberg <me@romanfreiberg.com>
2024-03-10 17:18:05 +0100

    custom dap.lua files

--- init.lua
+++ init.lua
@@ -32,43 +32,60 @@ return {
   },
 
   polish = function()
-    -- load in module from .vim/dap.lua
-    -- set dap.adapters = ...
+    local dap = require('dap')
 
-    -- if .vim/dap.lua does not exist use default configuration
-    local dap = require("dap")
-    dap.adapters.python = {
-      type = "executable",
-      command = "/usr/bin/python3",
-      args = { "-m", "debugpy.adapter" },
-    }
-    dap.configurations.python = {
-        {
-          type = "python", 
-          request = "launch",
-          name = "Launch file",
-          -- Options below are for debugpy, see https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings for supported options
-          program = "${file}", -- This configuration will launch the current file if used.
-          pythonPath = function()
-            local conda_prefix = os.getenv("CONDA_PREFIX")
-            if conda_prefix then
-              return conda_prefix .. "/bin/python"
-            else
-              local cwd = vim.fn.getcwd()
-              if vim.fn.executable(cwd .. "/venv/bin/python") == 1 then
-                return cwd .. "/venv/bin/python"
-              elseif vim.fn.executable(cwd .. "/.venv/bin/python") == 1 then
-                return cwd .. "/.venv/bin/python"
-              else
-                return "/usr/bin/python"
-              end
+    local function load_project_specific_dap()
+      local dap_config_path = vim.fn.getcwd() .. '/.vim/dap.lua'
+      if vim.fn.filereadable(dap_config_path) == 1 then
+        local project_dap = dofile(dap_config_path)
+        if project_dap and project_dap.dap then
+          if project_dap.dap.adapters then
+            for adapter_name, adapter_config in pairs(project_dap.dap.adapters) do
+              dap.adapters[adapter_name] = adapter_config
             end
-          end,
-          cwd = function()
-            return vim.fn.getcwd()
-          end,
-        },
-      -- other configurations...
-    }
+          end
+          if project_dap.dap.configurations then
+            for language, configs in pairs(project_dap.dap.configurations) do
+              dap.configurations[language] = configs
+            end
+          end
+        end
+      else
+        -- Fallback to default configuration if `.vim/dap.lua` does not exist
+        dap.adapters.python = {
+          type = "executable",
+          command = "/usr/bin/python3",
+          args = { "-m", "debugpy.adapter" },
+        }
+        dap.configurations.python = {
+          {
+            type = "python",
+            request = "launch",
+            name = "Launch file",
+            program = "${file}",
+            pythonPath = function()
+              local conda_prefix = os.getenv("CONDA_PREFIX")
+              if conda_prefix then
+                return conda_prefix .. "/bin/python"
+              else
+                local cwd = vim.fn.getcwd()
+                if vim.fn.executable(cwd .. "/venv/bin/python") == 1 then
+                  return cwd .. "/venv/bin/python"
+                elseif vim.fn.executable(cwd .. "/.venv/bin/python") == 1 then
+                  return cwd .. "/.venv/bin/python"
+                else
+                  return "/usr/bin/python"
+                end
+              end
+            end,
+            cwd = function()
+              return vim.fn.getcwd()
+            end,
+          },
+        }
+      end
+    end
+
+    load_project_specific_dap()
   end,
 }
